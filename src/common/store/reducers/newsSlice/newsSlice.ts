d29475d545903b281aadd474df2cc7fbac4ab32c
Den Pol <selection4test@gmail.com>
2024-12-27 01:41:48 +0300

    updating list fix

--- src/common/store/reducers/newsSlice/newsSlice.ts
+++ src/common/store/reducers/newsSlice/newsSlice.ts
@@ -106,9 +106,11 @@ const newsSlice = createSlice({
           state.persistedFavorites.unshift(action.payload.id)
           break
       }
+      state.pollingCounter += 1
     },
     removeFromPersistedFavorites: (state, action: PayloadAction<{ id: number; }>) => {
       state.persistedFavorites = state.persistedFavorites.filter((id) => id !== action.payload.id)
+      state.pollingCounter += 1
     },
   },
 });
